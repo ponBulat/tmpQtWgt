8b34a184fa0eac84f154e8bcf7a7cafaf0e26588
ponBulat <ponomarev@laser-bulat.ru>
2022-10-10 14:07:52 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include <QMatrix4x4>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -15,25 +16,59 @@ void MainWindow::paintEvent(QPaintEvent *event)
     Q_UNUSED(event)
 
     QPainter painter{this};
-
-    QPointF center{100, 100};
-    painter.setPen( QPen(Qt::red, 3) );
-    painter.drawPoint(center);
-
-    // -------------
-
     painter.setPen( QPen(Qt::black, 2) );
 
-    double pointCount = 16;
-    double radius = 50;
 
-    for( int i{0}; i < pointCount ; ++i) {
+    //входные параметры
+    float X1 = 200; //начальная точка (текущая позиция)
+    float Y1 = 250;
+    float X2 = 250;  //конечная точка (X,Y в G2 G3)
+    float Y2 = 200;
+    float XI = 250;  //центр поворота (I,J в G2 G3)
+    float YJ = 250;
+    bool clockwise = true; //G2 - по часовой, G3 - против часовой
+    float angle_step = 5;
 
-        qreal angle = qDegreesToRadians(180.0*i/pointCount);
+    QVector3D P1(X1-XI,Y1-YJ,0); //создаем 2 вектора
+    QVector3D P2(X2-XI,Y2-YJ,0);
 
-        qreal x = center.x() + radius * sin(angle);
-        qreal y = center.y() + radius * cos(angle);
+    float angle = 0;    //угол между векторами
 
-        painter.drawPoint(QPointF(x, y));
+    if(P1.length() && P2.length()){ //если вектора имеют длинну, смотрим угол
+        if(P1 == P2){ //если начальная и конечная точка одна и таже, то это окружность
+            (clockwise? angle = -360 : angle = 360);
+        }else{
+            P1.normalize(); //нормализуем их (приводим к единичной длинне, нам от векторов нужен только угол)
+            P2.normalize();
+
+            angle = qRadiansToDegrees( qAcos(QVector3D::dotProduct(P1,P2)) ); //из скалярного произведения векторов вытаскиваем угол
+
+            if((QVector3D::crossProduct(P1,P2)).z() < 0){     //если при этом поменялся знак детерминанта, то угол был больше 180, делаем поправку
+                angle = 360 - angle;
+            }
+
+            if(clockwise){ //рассчитанный угол был для движения против часовой стрелки, если это не так, то меняем направление (угол будет отрицательным)
+                angle = angle - 360;
+            }
+        }
+    }else{
+        angle = 0; //если вектора нулевые, то просто точка будет
+    }
+
+
+    QVector4D PR(X1-XI,Y1-YJ,0,1); //Координаты первой точки от центра вращения
+
+    float cur_angle = 0;
+
+    while( ( (!clockwise) && (cur_angle <= angle) ) ||
+           ( (clockwise) && (cur_angle >= angle) ) ){
+        QMatrix4x4 matrix; //матрицу разварота формируем для каждого угла, чтобы не потерять точность при накомплении точек
+
+        matrix.translate(XI,YJ,0); //смещаем центр вращения в координаты центра дуги
+        matrix.rotate(clockwise? cur_angle -= angle_step : cur_angle += angle_step, 0.0, 0.0, 1.0); //расчет текущего угла поворота
+
+        QVector4D P = matrix * PR; //получаем новую точку из первой с учетом угла
+
+        painter.drawPoint(P.toPointF()); //На экране Y координата перевернута
     }
 }
